2fe9282354bb07630f9c85ada8041ebb3d0d3d0e
leandro-guimaraes <74996502+sushi-jack@users.noreply.github.com>
2022-10-24 01:09:56 -0300

    pedido alterado

---
+++ Back end/AbsolutoGas/Controllers/TipoPagamento.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbsolutoGas.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class TipoPagamento : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult BuscarTodos()
+        {
+            var tipoPagamento = repositorioTipoPagamento.BuscarTodos();
+
+            if (tipoPagamento == null || !tipoPagamento.Qualquer())
+                return NotFound(new { mensage = $" Lista vazia. " });
+
+            retornar Ok( );
+
+        }
+    }
+}
